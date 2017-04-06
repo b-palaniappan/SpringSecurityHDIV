84297293d7d61b42b4b29aae81158cb2c0153091
b-palaniappan <b.palaniappan@hotmail.com>
2017-04-06 18:31:44 -0500

    Updated Test Case

--- src/test/java/io/c12/bala/service/UserServiceImplTest.java
+++ src/test/java/io/c12/bala/service/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package io.c12.bala.service;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
@@ -25,7 +26,12 @@ public class UserServiceImplTest {
 
 	@Test
 	public void authenticateUser() {
+		// Positive Test Case
 		when(userDao.getPasswordHashByUserId(userId)).thenReturn("$s0$41010$dAiki5TNWpASH54dazRjHg==$ogG51PevlOjcRHNkFIlprqMxUuzwJcvX7s8h5h2oMUI=");
 		assertTrue(userService.authenticateUser(userId, password));
+		
+		// Negative Test Case
+		when(userDao.getPasswordHashByUserId(userId)).thenReturn("$s0$41010$YHtdaf1NcTXeP6Uv+SVptA==$ZuDOHKVFsPNodTKec5C6JdSdojfh6NdKzEp/hm0uSHE=");
+		assertFalse(userService.authenticateUser(userId, password));
 	}
 }
