fedfd58f83339056dcdda1a0fd2f9cb6041b8d5e
b-palaniappan <b.palaniappan@hotmail.com>
2017-04-27 11:37:37 -0500

    clean up

--- src/main/java/io/c12/bala/spring/security/auth/CustomAuthenticationProvider.java
+++ src/main/java/io/c12/bala/spring/security/auth/CustomAuthenticationProvider.java
@@ -30,7 +30,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import io.c12.bala.service.UserService;
